00b83ec34b1748883ba0ec1c77258df180364b20
RashikaKarki <rashikakarki9841@gmail.com>
2020-02-06 22:20:10 +0545

    added scrape tool

---
+++ scrape.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests 
+from PIL import Image
+import os
+from io import BytesIO
+import re
+
+def scrape_image():
+    search=input("Search for:")
+    params={"q":search}
+    r=requests.get("https://www.bing.com/images/search",params=params)
+
+    soup=BeautifulSoup(r.text,"html.parser")
+    bimg = re.compile("mm.bing.net")
+    links = soup.find_all("img", {"src": bimg})
+    try:
+        path = os.path.join("./",search) 
+        os.mkdir(path)
+        i=0
+        for item in links:
+            i+=1
+            img_obj=requests.get(item.attrs["src"])
+            img=Image.open(BytesIO(img_obj.content))
+            img.save("./"+search+"/"+search+str(i)+"."+img.format)
+    except:
+        print("Could not create folder check if folder already exists")
+
+
+
+
+while True:
+    answer=input("Do you want to scrape image? (Y/N)")
+    if answer=="Y" or answer=="y" :
+        scrape_image()
+    elif answer=="N" or answer=="n" :
+        print("Come Back Soon")
+        break
+    else: 
+        print("Invalid Answer")
